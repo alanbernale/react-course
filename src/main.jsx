9fe5dac12068ac35e64f1226a3a75e43a79dcbeb
Alan Bernal <abernalespinoza@outlook.com>
2023-01-13 14:25:46 -0500

    Fundamentos de jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 function Greeting() {
-    return <div>
-        <h1>Este es un componente.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </div>
+    const user = {
+        fistName: 'Alan',
+        lastName: 'Bernal'
+    }
+
+    return <>
+        <h1>{user.fistName}</h1>
+        <h3>{user.lastName}</h3>
+    </>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <div>
+    <>
         <Greeting/>
         <Greeting/>
         <Greeting/>
-        <Greeting/>
-    </div>
+    </>
 )
