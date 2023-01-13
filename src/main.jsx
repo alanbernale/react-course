531426b1f15b197ab5a9db154481f65e92f06932
Alan Bernal <abernalespinoza@outlook.com>
2023-01-13 14:48:09 -0500

    Modulos de Javascript - EcmaScript.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-
-function Greeting() {
-    const user = {
-        fistName: 'Alan',
-        lastName: 'Bernal'
-    }
-
-    return <>
-        <h1>{user.fistName}</h1>
-        <h3>{user.lastName}</h3>
-    </>
-}
+import {Greeting, UserCard} from "./Greeting"
+import Product, {Navbar} from "./Product"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <Greeting/>
-        <Greeting/>
-        <Greeting/>
+        <UserCard/>
+        <Product/>
+        <Navbar/>
     </>
 )
