94718ed48532af4253c652ec368c3ec72aee7627
Alan Bernal <abernalespinoza@outlook.com>
2023-01-13 15:57:47 -0500

    PropTypes y DefaultProps.

---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+export function Button({text, name}) {
+    if (!text) console.error('Text is required.')
+
+    return <button>
+        {text} - {name}
+    </button>
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
+Button.defaultProps = {
+    name: 'Some User'
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import {Greeting, UserCard} from "./Greeting"
 import Product, {Navbar} from "./Product"
+import {Button} from "./Button"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-        <UserCard
-            name="Ryan Ray"
-            amount={3000}
-            married={true}
-            points={[99, 33.3, 22.2]}
-            address={{street: '123 Main Street', city: 'New York'}}
-            greet={function () {
-                alert('Hello')
-            }}
-        />
-
-        <UserCard
-            name="Joe McMillan"
-            amount={7400}
-            married={false}
-            points={[123, 22, 190]}
-            address={{street: '155 Lincoln Boulevard', city: 'Ohio'}}
-            greet={function () {
-                console.log('Ok')
-            }}
-        />
+        <Button text="Click Me" />
+        <Button text="Pay" />
+        <Button text="Go To" name="Joe"/>
     </>
 )
