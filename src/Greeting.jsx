047876af6d6f926272ac911d531072c7b0166d6e
Alan Bernal <abernalespinoza@outlook.com>
2023-01-13 15:22:11 -0500

    Fundamentos de React Props.

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,21 @@
-export function Greeting() {
-    return <h1>Componente de React</h1>
+export function Greeting({title, name = "User"}) {
+    return <h1>{title}, dice {name}</h1>
 }
 
-export function UserCard() {
-    return <h1>User Card</h1>
+export function UserCard({name, amount, married, points, address, greet}) {
+    return <div>
+        <h1>{name}</h1>
+        <p>💵{amount}</p>
+        <p>{married ? 'married' : 'single'}</p>
+        <p>Points:</p>
+        <ul>
+            {points.map((point) => <li key={point}>{point}</li>)}
+        </ul>
+        <p>Address:</p>
+        <ul>
+            <li>City: {address.city}</li>
+            <li>Street: {address.street}</li>
+        </ul>
+    </div>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,26 @@ import Product, {Navbar} from "./Product"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-        <Greeting/>
-        <UserCard/>
-        <Product/>
-        <Navbar/>
+        <UserCard
+            name="Ryan Ray"
+            amount={3000}
+            married={true}
+            points={[99, 33.3, 22.2]}
+            address={{street: '123 Main Street', city: 'New York'}}
+            greet={function () {
+                alert('Hello')
+            }}
+        />
+
+        <UserCard
+            name="Joe McMillan"
+            amount={7400}
+            married={false}
+            points={[123, 22, 190]}
+            address={{street: '155 Lincoln Boulevard', city: 'Ohio'}}
+            greet={function () {
+                console.log('Ok')
+            }}
+        />
     </>
 )
