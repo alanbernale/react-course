8d6b89f370e07817ef883f8085669fab54e88e19
Alan Bernal <abernalespinoza@outlook.com>
2023-01-15 18:48:33 -0500

    Tipos de componentes.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import {Greeting, UserCard} from "./Greeting"
 import Product, {Navbar} from "./Product"
 import {Button} from "./Button"
 import {TaskCard} from "./Task"
+import {Saludar} from "./Saludar"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-        <TaskCard ready={true}/>
-        <TaskCard ready={false}/>
+        <Saludar/>
     </>
 )
