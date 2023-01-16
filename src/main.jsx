03d927dd4b6a88bed9bb0e8674372686cbd88e74
Alan Bernal <abernalespinoza@outlook.com>
2023-01-16 12:06:03 -0500

    Arrays.

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,31 @@ import {TaskCard} from "./Task"
 import {Saludar} from "./Saludar"
 import {Posts} from "./Posts"
 
+const users = [
+    {
+        id: "1",
+        name: "Ryan Ray",
+        image: "https://robohash.org/user1"
+    },
+    {
+        id: "2",
+        name: "Joe Jonas",
+        image: "https://robohash.org/user2"
+    },
+    {
+        id: "3",
+        name: "Marthy McMillan",
+        image: "https://robohash.org/user3"
+    },
+]
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-        <Posts/>
+        {users.map((user) => {
+            return <div key={user.id}>
+                <h1>{user.name}</h1>
+                <img src={user.image} alt={user.name}/>
+            </div>
+        })}
     </>
 )
